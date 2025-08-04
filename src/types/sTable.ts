f421dca7d39f2d103f7435d0c82ee2568ffa074d
opteacher <opteacher@hotmail.com>
2025-08-04 17:27:35 +0800

    共享表格开发推进中

--- src/types/sTable.ts
+++ src/types/sTable.ts
@@ -1,3 +1,4 @@
+import Mapper, { EdtLstMapper } from '@lib/types/mapper'
 import { gnlCpy } from '@lib/utils'
 
 export default class STable {
@@ -19,5 +20,141 @@ export default class STable {
 
   static copy(src: any, tgt?: STable, force = false): STable {
     return gnlCpy(STable, src, tgt, { force })
+  }
+}
+
+export const avaCmpTypes = [
+  'Input',
+  'Number',
+  'Textarea',
+  'Select',
+  'Radio',
+  'Checkbox',
+  'Switch',
+  'UploadFile'
+]
+
+export const opnsMapper = {
+  label: '选项',
+  desc: '',
+  type: 'EditList',
+  placeholder: '',
+  lblProp: 'label',
+  subProp: 'value',
+  inline: false,
+  flatItem: false,
+  mapper: new Mapper({
+    label: {
+      type: 'Input',
+      label: '显示名',
+      placeholder: '输入选项值，不填则使用选项值'
+    },
+    value: {
+      type: 'Input',
+      label: '选项值',
+      placeholder: '输入选项值',
+      rules: [{ required: true, message: '必须给出值名！' }]
+    }
+  })
+} as EdtLstMapper
+
+export const extraDict = {
+  Input: {
+    prefix: {
+      type: 'Input',
+      label: '前缀',
+      placeholder: '输入输入框前缀'
+    },
+    suffix: {
+      type: 'Input',
+      label: '后缀',
+      placeholder: '输入输入框后缀'
+    }
+  },
+  Number: {
+    prefix: {
+      type: 'Input',
+      label: '前缀',
+      placeholder: '输入输入框前缀'
+    },
+    suffix: {
+      type: 'Input',
+      label: '后缀',
+      placeholder: '输入输入框后缀'
+    }
+  },
+  Textarea: {
+    maxRows: {
+      type: 'Number',
+      label: '最大行数',
+      placeholder: '输入最大行数'
+    }
+  },
+  Select: {
+    options: opnsMapper
+  },
+  Radio: {
+    options: opnsMapper,
+    style: {
+      type: 'Radio',
+      label: '样式',
+      options: [
+        {
+          label: '点式',
+          value: 'circle'
+        },
+        {
+          label: '按钮式',
+          value: 'button'
+        }
+      ]
+    }
+  },
+  Checkbox: {
+    options: opnsMapper,
+    chkLabels: {
+      type: 'Unknown',
+      label: '复选框标签'
+    }
+  },
+  Switch: {
+    chkLabels: {
+      type: 'Unknown',
+      label: '开关标签'
+    }
+  },
+  UploadFile: {
+    path: {
+      type: 'Input',
+      label: '上传URL地址'
+    },
+    params: {
+      type: 'EditList',
+      label: '上传参数',
+      lblProp: 'param',
+      subProp: 'value',
+      inline: true,
+      flatItem: false,
+      mapper: new Mapper({
+        param: {
+          type: 'Input',
+          label: '参数名',
+          placeholder: '输入参数名'
+        },
+        value: {
+          type: 'Input',
+          label: '参数值',
+          placeholder: '输入参数值'
+        }
+      })
+    },
+    directory: {
+      type: 'Switch',
+      label: '是否上传文件夹'
+    },
+    headers: {
+      type: 'JsonEditor',
+      label: '请求头'
+    }
   }
 }
--- src/views/ShareTable.vue
+++ src/views/ShareTable.vue
@@ -2,18 +2,24 @@
 import EditableTable from '@lib/components/EditableTable.vue'
 import Column from '@lib/types/column'
 import Mapper from '@lib/types/mapper'
-import { newOne } from '@lib/utils'
-import STable from '@/types/sTable'
-import { reactive } from 'vue'
-import { CloseOutlined } from '@ant-design/icons-vue'
+import { newOne, setProp, getProp } from '@lib/utils'
+import STable, { avaCmpTypes, extraDict } from '@/types/sTable'
+import { computed, reactive } from 'vue'
+import { compoOpns, cmpNickDict, type CompoType } from '@lib/types/index'
+import { cloneDeep } from 'lodash'
+import FormGroup from '@lib/components/FormGroup.vue'
+import { DeleteOutlined } from '@ant-design/icons-vue'
 
 const shareTable = reactive({
-  api: { all: () => [] },
+  api: {
+    all: () => []
+  },
   columns: [new Column('名称', 'name'), new Column('表单结构', 'form')],
   mapper: new Mapper({
     name: {
       type: 'Input',
-      label: '表格名称'
+      label: '表格名称',
+      rules: [{ required: true, message: '请输入表格名称' }]
     },
     form: {
       type: 'JsonEditor',
@@ -26,18 +32,71 @@ const tableDesign = reactive({
     {
       title: '新增列',
       dataIndex: 'addCol',
-      key: 'addCol'
+      key: 'addCol',
+      width: 120
     }
+  ] as Array<any>,
+  data: [
+    { addCol: '唯一标识' },
+    { addCol: '类型' },
+    { addCol: '提示' },
+    { addCol: '必填/必选' },
+    { addCol: '描述' },
+    { addCol: '额外参数' }
   ],
-  data: [{ addCol: '类型' }],
   form: {
-
-  }
+    key: '',
+    label: '',
+    type: 'Input',
+    placeholder: '',
+    required: false,
+    desc: '',
+    extra: {} as any
+  },
+  colMapper: null as any
 })
+const isNewCol = computed(() => tableDesign.columns.findIndex(col => col.key === 'newCol') !== -1)
 
 function onAddColClick() {
   if (!tableDesign.columns.find(col => col.key === 'newCol')) {
     tableDesign.columns.push({ title: '', dataIndex: 'newCol', key: 'newCol' })
+  } else {
+    onClsNewColClick()
+  }
+}
+function onClsNewColClick() {
+  const index = tableDesign.columns.findIndex(col => col.key === 'newCol')
+  if (index !== -1) {
+    tableDesign.columns.splice(index, 1)
+  }
+}
+function onNewColSubmit(stable: STable) {
+  tableDesign.columns.push({
+    title: tableDesign.form.label,
+    dataIndex: tableDesign.form.key,
+    key: tableDesign.form.key,
+    mapper: cloneDeep(tableDesign.form)
+  })
+  stable.form = Object.fromEntries(
+    tableDesign.columns
+      .filter(col => !['addCol', 'newCol'].includes(col.key))
+      .map(col => [col.key, cloneDeep(col.mapper)])
+  )
+  resetTblDsgnForm()
+  onClsNewColClick()
+}
+function resetTblDsgnForm() {
+  tableDesign.form.key = ''
+  tableDesign.form.label = ''
+  tableDesign.form.type = 'Input'
+  tableDesign.form.required = false
+  tableDesign.form.desc = ''
+  tableDesign.form.extra = {}
+}
+function onDelColSubmit(column: any) {
+  const index = tableDesign.columns.findIndex(col => col.key === column.key)
+  if (index !== -1) {
+    tableDesign.columns.splice(index, 1)
   }
 }
 </script>
@@ -45,33 +104,158 @@ function onAddColClick() {
 <template>
   <EditableTable
     title="共享表格"
+    :dlg-full-scrn="true"
     :api="shareTable.api"
     :columns="shareTable.columns"
     :mapper="shareTable.mapper"
     :new-fun="() => newOne(STable)"
   >
-    <template #formEDT>
-      <a-table :data-source="tableDesign.data" :columns="tableDesign.columns" :pagination="false">
-        <template #headerCell="{ column }">
-          <template v-if="column.key === 'addCol'">
-            <a-button type="link" @click="onAddColClick">新增列</a-button>
-          </template>
-          <template v-else-if="column.key === 'newCol'">
-            <a-input class="flex-1" v-model:value="column.title" placeholder="请输入列名">
-              <template #suffix>
-                <a-button type="text" danger size="small">
-                  <template #icon><CloseOutlined /></template>
+    <template #formEDT="{ editing }: any">
+      <a-form-item-rest>
+        <a-table :data-source="tableDesign.data" :columns="tableDesign.columns" :pagination="false">
+          <template #headerCell="{ column }">
+            <template v-if="column.key === 'addCol'">
+              <a @click="onAddColClick">
+                <a-space align="center">
+                  <span class="text-[#ff4d4f] text-xl">*</span>
+                  新增列
+                </a-space>
+              </a>
+            </template>
+            <template v-else-if="column.key === 'newCol'">
+              <a-input
+                class="flex-1"
+                placeholder="请输入列名（中文）"
+                v-model:value="tableDesign.form.label"
+              />
+            </template>
+            <template v-else>
+              {{ column.title }}
+              <a-popconfirm title="确定删除该列" @confirm="() => onDelColSubmit(column)">
+                <a-button type="text" danger>
+                  <template #icon><DeleteOutlined /></template>
                 </a-button>
+              </a-popconfirm>
+            </template>
+          </template>
+          <template #bodyCell="{ column, record }">
+            <template
+              v-if="column.key === 'addCol' && ['唯一标识', '类型'].includes(record.addCol)"
+            >
+              <a-space align="center">
+                <span class="text-[#ff4d4f] text-xl">*</span>
+                {{ record.addCol }}
+              </a-space>
+            </template>
+            <template v-else-if="column.key === 'newCol'">
+              <a-input
+                v-if="record.addCol === '唯一标识'"
+                placeholder="输入唯一标识（英文）"
+                v-model:value="tableDesign.form.key"
+              />
+              <a-select
+                v-else-if="record.addCol === '类型'"
+                placeholder="选择类型"
+                :options="compoOpns.filter(opn => avaCmpTypes.includes(opn.value))"
+                v-model:value="tableDesign.form.type"
+              />
+              <a-input
+                v-else-if="record.addCol === '提示'"
+                placeholder="输入表单框中提示"
+                v-model:value="tableDesign.form.placeholder"
+              />
+              <a-switch
+                v-else-if="record.addCol === '必填/必选'"
+                v-model:checked="tableDesign.form.required"
+              />
+              <a-textarea
+                v-else-if="record.addCol === '描述'"
+                placeholder="输入描述"
+                v-model:value="tableDesign.form.desc"
+              />
+              <a-form v-else-if="record.addCol === '额外参数'">
+                <FormGroup
+                  :mapper="new Mapper(getProp(extraDict, tableDesign.form.type) || {})"
+                  :form="tableDesign.form.extra"
+                  @update:fprop="(e: any) => Object.assign(tableDesign.form.extra, e)"
+                >
+                  <template #chkLabels>
+                    <a-form-item-rest>
+                      <a-input-group compact class="flex">
+                        <a-input
+                          class="flex-1 border-e-0 focus:shadow-none"
+                          placeholder="为真时的文本说明"
+                        />
+                        <a-input class="bg-white px-0 w-2 border-x-0" disabled placeholder="/" />
+                        <a-input
+                          class="flex-1 border-s-0 focus:shadow-none"
+                          placeholder="为假时的文本说明"
+                        />
+                      </a-input-group>
+                    </a-form-item-rest>
+                  </template>
+                </FormGroup>
+              </a-form>
+            </template>
+            <template v-else-if="column.mapper">
+              <a-typography-text v-if="record.addCol === '唯一标识'">
+                {{ column.mapper.key }}
+              </a-typography-text>
+              <a-typography-text v-else-if="record.addCol === '类型'">
+                {{ cmpNickDict[column.mapper.type as CompoType] }}
+              </a-typography-text>
+              <a-typography-text v-else-if="record.addCol === '提示'">
+                {{ column.mapper.placeholder || '无' }}
+              </a-typography-text>
+              <a-typography-text v-else-if="record.addCol === '必填/必选'">
+                {{ column.mapper.required ? '必填/必选' : '非必要' }}
+              </a-typography-text>
+              <a-typography-text v-else-if="record.addCol === '描述'">
+                {{ column.mapper.desc || '无' }}
+              </a-typography-text>
+              <template v-else-if="record.addCol === '额外参数'">
+                <a-popover trigger="click">
+                  <template #content>
+                    <FormGroup
+                      class="w-96"
+                      :lbl-wid="4"
+                      :mapper="new Mapper(getProp(extraDict, column.mapper.type))"
+                      :form="column.mapper.extra"
+                      :viewOnly="true"
+                    >
+                      <template #optionsVW>
+                        <ul>
+                          <li v-for="option in column.mapper.extra.options">
+                            {{ option.label }}&nbsp;
+                            <span class="text-gray-400">{{ option.value }}</span>
+                          </li>
+                        </ul>
+                      </template>
+                    </FormGroup>
+                  </template>
+                  <a-button @click="() => setProp(tableDesign, 'colMapper', column.mapper)">
+                    显示额外参数
+                  </a-button>
+                </a-popover>
               </template>
-            </a-input>
+            </template>
           </template>
-        </template>
-        <template #bodyCell="{ column }">
-          <template v-if="column.key === 'newCol'">
-            <a-select />
+          <template v-if="isNewCol" #footer>
+            <div class="text-right">
+              <a-space>
+                <a-button type="primary" @click="() => onNewColSubmit(editing)">确定</a-button>
+                <a-button @click="onClsNewColClick">取消</a-button>
+              </a-space>
+            </div>
           </template>
-        </template>
-      </a-table>
+        </a-table>
+      </a-form-item-rest>
     </template>
   </EditableTable>
 </template>
+
+<style>
+.ant-form-item:last-child {
+  margin-bottom: 0 !important;
+}
+</style>
